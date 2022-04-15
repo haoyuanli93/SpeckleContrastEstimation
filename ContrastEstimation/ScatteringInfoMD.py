c76cd2f5ada68608530cab41abac6cbf452633e5
Haoyuan Li <hyli16@stanford.edu>
2022-04-15 09:28:44 -0700

    minor

--- ContrastEstimation/ScatteringInfoMD.py
+++ ContrastEstimation/ScatteringInfoMD.py
@@ -2,6 +2,7 @@ import numpy as np
 from numba import cuda
 import math
 from ContrastEstimation.AtomFormFactor import get_atomic_formfactor
+from ContrastEstimation.util import re0
 
 
 def load_atom_info(file_name):
@@ -23,7 +24,7 @@ def load_atom_info(file_name):
         ################################
         # Get the box size
         ################################
-        my_line = data_file.readline()
+        _ = data_file.readline()
 
         my_line = data_file.readline().split()
         xlo = float(my_line[0])
@@ -64,10 +65,6 @@ def categorize_atoms(atom_types, position_holder):
     :param position_holder:
     :return:
     """
-
-    # Get different number of atom_types
-    atom_type_list, atom_type_count = np.unique(atom_types, return_counts=True)
-    atom_type_num = atom_type_list.shape[0]
 
     # Sort the atom_types array based on the type
     sorted_idx = np.argsort(atom_types)
@@ -130,10 +127,10 @@ def get_q_vector_list_in_range(box_size_xyz_A, q_low_A, q_high_A):
 #
 #     I borrow this piece of code from my previous implementation of the pysingfel
 ###############################################################
-@cuda.jit('void(float64[:], float64[:], float64[:,:], float64[:,:], float64[:,:], float64[:], int64, int64, int64)')
-def _get_diffracted_field_at_Q_list(cos_holder, sin_holder,
-                                    form_factor_list, q_list, atom_position,
-                                    split_idx, atom_type_num, q_num, atom_num):
+@cuda.jit('void(float64[:], float64[:], float64[:,:], float64[:,:], float64[:,:], int64[:], int64, int64, int64)')
+def _get_MD_formfactor_at_Q_list(cos_holder, sin_holder,
+                                 form_factor_list, q_list, atom_position,
+                                 split_idx, atom_type_num, q_num, atom_num):
     atom_iter = cuda.grid(1)
     if atom_iter < atom_num:
 
@@ -154,7 +151,7 @@ def _get_diffracted_field_at_Q_list(cos_holder, sin_holder,
             cuda.atomic.add(sin_holder, q_idx, form_factor * math.sin(phase))
 
 
-def get_diffracted_field_at_Q_list(q_list_A, atom_position_array, atom_type_array, atom_type_name_list):
+def get_MD_formfactor_at_Q_list(q_list_A, atom_position_array, atom_type_array, atom_type_name_list):
     """
 
     :param q_list_A:
@@ -166,7 +163,7 @@ def get_diffracted_field_at_Q_list(q_list_A, atom_position_array, atom_type_arra
 
     # convert the reciprocal space into a 1d series.
     q_len_array = np.linalg.norm(q_list_A, axis=-1)
-    q_num = q_list_A.shape
+    q_num = q_list_A.shape[0]
 
     # Organize the atom info
     (atom_type_unique,
@@ -187,14 +184,114 @@ def get_diffracted_field_at_Q_list(q_list_A, atom_position_array, atom_type_arra
     sin_holder = np.zeros(q_num, dtype=np.float64)
 
     # Calculate the pattern
-    _get_diffracted_field_at_Q_list[(q_num + 511) / 512, 512](cos_holder,
-                                                              sin_holder,
-                                                              form_factor_list,
-                                                              np.ascontiguousarray(q_list_A),
-                                                              atom_position_sorted,
-                                                              atom_type_start_point,
-                                                              len(atom_type_name_list),
-                                                              q_num,
-                                                              atom_type_array.shape[0])
+    _get_MD_formfactor_at_Q_list[(q_num + 511) // 512, 512](cos_holder,
+                                                            sin_holder,
+                                                            form_factor_list,
+                                                            np.ascontiguousarray(q_list_A),
+                                                            atom_position_sorted,
+                                                            atom_type_start_point,
+                                                            len(atom_type_name_list),
+                                                            q_num,
+                                                            atom_type_array.shape[0])
 
     return cos_holder + 1.j * sin_holder
+
+
+@cuda.jit('void(float64[:], float64[:], float64[:,:], float64[:,:], float64[:,:], int64[:], int64, int64, int64)')
+def _get_MD_formfactor_at_Q_list_parallel_at_Q(cos_holder, sin_holder,
+                                               form_factor_list, q_list, atom_position,
+                                               split_idx, atom_type_num, q_num, atom_num):
+    q_idx = cuda.grid(1)
+    if q_idx < q_num:
+
+        for atom_type_idx in range(atom_type_num):
+            form_factor = form_factor_list[atom_type_idx, q_idx]
+
+            for atom_idx in range(split_idx[atom_type_idx], split_idx[atom_type_idx + 1]):
+                phase = (q_list[q_idx, 0] * atom_position[atom_idx, 0] +
+                         q_list[q_idx, 1] * atom_position[atom_idx, 1] +
+                         q_list[q_idx, 2] * atom_position[atom_idx, 2])
+
+                cuda.atomic.add(cos_holder, q_idx, form_factor * math.cos(phase))
+                cuda.atomic.add(sin_holder, q_idx, form_factor * math.sin(phase))
+
+
+def get_MD_formfactor_at_Q_list_parallel_at_Q(q_list_A, atom_position_array, atom_type_array, atom_type_name_list):
+    """
+
+    :param q_list_A:
+    :param atom_position_array:
+    :param atom_type_array:
+    :param atom_type_name_list:
+    :return:
+    """
+
+    # convert the reciprocal space into a 1d series.
+    q_len_array = np.linalg.norm(q_list_A, axis=-1)
+    q_num = q_list_A.shape[0]
+    atom_num = atom_type_array.shape[0]
+
+    # Organize the atom info
+    (atom_type_unique,
+     atom_type_start_point,
+     atom_type_count,
+     atom_type_sorted,
+     atom_position_sorted) = categorize_atoms(atom_types=atom_type_array, position_holder=atom_position_array)
+    atom_type_num = len(atom_type_name_list)
+
+    # Construct the split idx
+    split_idx = np.zeros(atom_type_start_point.shape[0] + 1, dtype=np.int64)
+    split_idx[:atom_type_start_point.shape[0]] = atom_type_start_point[:]
+    split_idx[-1] = atom_type_array.shape[0]
+
+    # Get the form factor of each atom at each reciprocal point
+    form_factor_list = np.zeros((atom_type_num, q_num), dtype=np.float64)
+    for atom_type_idx in range(atom_type_num):
+        for q_idx in range(q_num):
+            form_factor_list[atom_type_idx, q_idx] = get_atomic_formfactor(atom_name=atom_type_name_list[atom_type_idx],
+                                                                           q_detector_in_A=q_len_array[q_idx])
+
+    # create
+    cos_holder = np.zeros(q_num, dtype=np.float64)
+    sin_holder = np.zeros(q_num, dtype=np.float64)
+
+    # Calculate the pattern
+    _get_MD_formfactor_at_Q_list_parallel_at_Q[(q_num + 511) // 512, 512](cos_holder,
+                                                                          sin_holder,
+                                                                          form_factor_list,
+                                                                          q_list_A,
+                                                                          atom_position_sorted,
+                                                                          split_idx,
+                                                                          atom_type_num,
+                                                                          q_num,
+                                                                          atom_num)
+
+    return cos_holder + 1.j * sin_holder
+
+
+def get_diffracted_flux_with_MD_formfactor(in_flux, dOmega, q_in, d_eff_m, box_size_A,
+                                           q_list_A, formfactorMD):
+    """
+
+    :param in_flux:
+    :param dOmega:
+    :param q_in:
+    :param d_eff_m:
+    :param box_size_A:
+    :param q_list_A:
+    :param formfactorMD:
+    :return:
+    """
+
+    # convert the reciprocal space into a 1d series.
+    q_len_array = np.linalg.norm(q_list_A, axis=-1)
+
+    # Get the polarization factor
+    polarization_factor = np.square(1 - 0.5 * np.square(q_len_array / 2. / q_in))
+
+    # Get the intensity
+    intensity = np.multiply(np.square(np.abs(formfactorMD)), polarization_factor)
+    intensity *= (in_flux * dOmega * re0 ** 2)
+    intensity *= (d_eff_m * 1e10 / (box_size_A[2, 1] - box_size_A[2, 0]))
+
+    return intensity
